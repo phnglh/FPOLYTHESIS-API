df035aa5eb8ffc3ff6c701bcc963183624c26c8e
Le Hong Phong <phonghngle@gmail.com>
2024-12-21 19:10:37 +0700

    create base-request

--- app/Http/Requests/BaseRequest.php
+++ app/Http/Requests/BaseRequest.php
@@ -1,16 +1,20 @@
 <?php
+
 namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+
 class BaseRequest extends FormRequest
 {
-public function response(array $errors)
-{
-throw new HttpResponseException(response()->json([
-'success' => false,
-'errors' => [
-'message' => $errors
-]
-], 422));
-}
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'status' => 'error',
+                'errors' => $validator->errors()->first()
+            ], 422)
+        );
+    }
 }
