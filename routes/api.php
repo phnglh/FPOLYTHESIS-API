1b1073c5f26b822c3164bef058d2995fd84b5bc5
Le Hong Phong <phonghngle@gmail.com>
2025-02-07 10:29:22 +0700

    remove route

--- routes/api.php
+++ routes/api.php
@@ -40,11 +40,3 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
     });
 });
-
-Route::prefix('caterories')->group(function(){
-    Route::get('/', [CategoryController::class, 'index']);
-    Route::post('/', [CategoryController::class, 'store']);
-    Route::get('/{$id}', [CategoryController::class, 'show']);
-    Route::put('/{$id}', [CategoryController::class, 'update']);
-    Route::delete('/{$id}', [CategoryController::class, 'destroy']);
-});
